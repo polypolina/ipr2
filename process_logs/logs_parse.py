d5a9b5b7ae0a29393826bc6da1be99eb8b19f322
Polina <polinedoe@gmail.com>
2022-08-30 16:02:30 +0300

    fixed log parser

--- process_logs/logs_parse.py
+++ process_logs/logs_parse.py
@@ -2,20 +2,15 @@ import re
 import gzip
 import io
 import sys
+import csv
 
 filename = sys.argv[1]
 
 with open('log.csv', 'w') as fout:
+    writer = csv.writer(fout, delimiter=';')
     with gzip.open(filename, 'rb') as f:
         with io.TextIOWrapper(f, encoding='utf-8') as decoder:
             for line in decoder:
                 s = re.split('\s', line)
-                n = len(s)
-                for i in range(n-1):
-                    fout.write(s[i]+";")
-                fout.write(s[n-1])
-                fout.write("\n")
-
-
-
-
+                writer.writerow(s)
+                
