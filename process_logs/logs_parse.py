59cbb6b4930964508c83fceab148f858566205f3
Polina <polinedoe@gmail.com>
2022-08-31 18:26:33 +0300

    fix log parser

--- process_logs/logs_parse.py
+++ process_logs/logs_parse.py
@@ -6,11 +6,16 @@ import csv
 
 filename = sys.argv[1]
 
+pattern = r'(\w*\s+\d*\s\d{2}:\d{2}:\d{2})\s(\S*)\s(\S*):\s(\S*\s.{5})\s(\S*)\s-\s(\S*)\s(\S*)\s(\S*)\s"(\S*)"\s(\S*)\s(\S*)\s(\S*)\s"(\S*)"\s"(.*)"\s-\s(\S*):\s(\S*)\n$'
+
 with open('log.csv', 'w') as fout:
     writer = csv.writer(fout, delimiter=';')
     with gzip.open(filename, 'rb') as f:
         with io.TextIOWrapper(f, encoding='utf-8') as decoder:
             for line in decoder:
-                s = re.split('\s', line)
-                writer.writerow(s)
-                
+                s = re.findall(pattern, line)
+                if s == []:
+                    continue
+                result = s[0]
+                writer.writerow(result)
+
